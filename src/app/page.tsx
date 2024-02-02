64d61d4fc5b1e178dcea4fbd6fc85be0ab951134
marcosuth <marcsouthofficial@gmail.com>
2024-02-01 23:47:54 -0500

    lets see on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import AboutSection from "./components/AboutSection"
 export default function Home() {
   return (
 <>
-<div className="relative flex py-12 flex-col items-center w-full h-96 px-4">
-  <div className="absolute inset-0 bg-[url('/images/bbblurry.svg')] bg-cover bg-top w-full bg-no-repeat  max-sm:bg-contain"></div>
+<div className="relative flex py-6  flex-col items-center w-full h-96 px-4">
+  <div className="absolute inset-0 bg-[url('/images/landing-to-try.jpg')] bg-cover bg-top w-full bg-no-repeat brightness-50 "></div>
 
   {/* Text elements */}
   <div className="relative z-41">
-    <h1 className="font-bold text-6xl text-center px-4 dark"> MARC<span className="bg-gradient-to-r from-sky-400 to-violet-600 bg-clip-text text-transparent"> VINOKUROV </span></h1>
-    <p className="text-center  mt-2">Front-end Development and Software Portfolio</p>
+    <h1 className="font-bold text-6xl text-center px-4 text-white"> MARC<span className="bg-gradient-to-r from-sky-400 to-violet-600 bg-clip-text text-transparent"> VINOKUROV </span></h1>
+    <p className="text-center text-white  mt-2">Front-end Development and Software Portfolio</p>
   </div>
 </div>
 
